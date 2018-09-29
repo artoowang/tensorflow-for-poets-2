9f8c9c4a0d238239597e259a0abf59fb26751394
Chun-Po Wang <artoowang@gmail.com>
2018-09-28 22:40:20 -0700

    Changed script to run commands directly.
    Write to file means I need to worry about escape strings.

--- brand_model_to_brand.sh
+++ brand_model_to_brand.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Iterates through all the directories under <base_dir> and the images, creates
+# Iterates through all the directories under <base_dir> and the images, runs
 # a list of mkdir commands to create unique brand names under <output_dir>, and
-# then creates a list of mv commands to move all the images under <base_dir> to
+# then runs a list of mv commands to move all the images under <base_dir> to
 # their corresponding brand dir under <output_dir>. E.g., for
 #
 # <base_dir>/'Acura' 'CL'/1.jpg
 # <base_dir>/'Acura' 'CL'/2.jpg
 # <base_dir>/'Acura' 'Integra'/1.jpg
 #
-# this script outputs
+# this script runs
 #
 # mkdir -p <output_dir>/Acura
 # mv <base_dir>/'Acura' 'CL'/1.jpg <output_dir>/Acura/
@@ -24,8 +24,7 @@ fi
 base_dir="$1"
 output_dir="$2"
 
-# Print a list of mkdir commands for each unique brand in $base_dir, created
-# under $output_dir.
+# Run mkdir commands.
 brands_txt=""
 for dir in "$base_dir"/*; do
     dir_name=$(basename "$dir")
@@ -34,15 +33,16 @@ for dir in "$base_dir"/*; do
 done
 
 printf "$brands_text" | sort | uniq | while read brand; do
-    printf "mkdir -p \"%s/%s\"\n" "$output_dir" "$brand"
+    echo mkdir -p "$output_dir"/"$brand"
+    mkdir -p "$output_dir"/"$brand"
 done
 
-# Print a list of mv commands to move all photos into their corresponding
-# brand dir under $output_dir.
+# Run mv commands.
 for dir in "$base_dir"/*; do
     dir_name=$(basename "$dir")
     brand="$(echo "$dir_name" | sed "s/^'\([^']*\)'.*/\1/")"
     for img in "$dir"/*; do
-        printf "mv \"%s\" \"%s/%s/\"\n" "$img" "$output_dir" "$brand"
+        echo mv "$img" "$output_dir"/"$brand"
+        mv "$img" "$output_dir"/"$brand"
     done
 done
